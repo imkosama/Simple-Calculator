9e9f1c5fdf86c5a71db46c860b8242e6b92445a6
Osama khan <88250363+imkosama@users.noreply.github.com>
2023-02-18 01:00:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# Simple calculator 
+
+a=int(input('Enter the  First number :'))
+b=int(input('Enter the second number : '))
+
+print('The addition :',a+b)
+print('The multplication :',a*b)
+print('The divison :',a/b)
+print('The module :',a%b)
+print('The subtraction :',a-b)
+print('The exponential :',a**b)
+print('The floor divison:',a//b) # It will remove the float number and only give decimal number (2.5 -> 2 )
